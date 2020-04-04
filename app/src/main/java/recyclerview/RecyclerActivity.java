6527470e65edc856a283344389fd695b9cd25afb
dhoyne <darrenhoyne@gmail.com>
2020-04-04 20:46:10 +0100

    Recycler view tutorial

---
+++ app/src/main/java/recyclerview/RecyclerActivity.java
@@ -0,0 +1,55 @@
+package recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+//import com.google.android.material.appbar.AppBarLayout;
+
+import com.example.learning.R;
+
+import java.util.List;
+
+import recyclerview.model.NoteInfo;
+import recyclerview.model.NoteRecyclerAdapter;
+
+public class RecyclerActivity extends AppCompatActivity {
+
+    NoteRecyclerAdapter noteRecyclerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(NoteListActivity.this, NoteActivity.class));
+//            }
+//        });
+
+        initializeDisplayContent();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        noteRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initializeDisplayContent() {
+        final RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        final LinearLayoutManager noteLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(noteLayoutManager);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        noteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
+        recyclerView.setAdapter(noteRecyclerAdapter);
+    }
+}
---
+++ app/src/main/java/recyclerview/model/NoteRecyclerAdapter.java
@@ -0,0 +1,74 @@
+package recyclerview.model;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.learning.R;
+
+import java.util.List;
+
+import recyclerview.NoteActivity;
+
+public class NoteRecyclerAdapter extends RecyclerView.Adapter<NoteRecyclerAdapter.ViewHolder>{
+
+    private final Context context;
+    private final List<NoteInfo> notesList;
+    LayoutInflater layoutInflater;
+
+    public NoteRecyclerAdapter(Context context, List<NoteInfo> notesList) {
+        this.context = context;
+         layoutInflater = LayoutInflater.from(context);
+        this.notesList = notesList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = layoutInflater.inflate(R.layout.item_note_list,parent,false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final NoteInfo noteInfo = notesList.get(position);
+        holder.courseTextView.setText(noteInfo.getCourse().getTitle());
+        holder.titleTextView.setText(noteInfo.getTitle());
+        holder.currentPosition = position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView courseTextView;
+        public TextView titleTextView;
+        public int currentPosition;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+             courseTextView = itemView.findViewById(R.id.text_course);
+             titleTextView = itemView.findViewById(R.id.text_title);
+
+             itemView.setOnClickListener(new View.OnClickListener() {
+                 @Override
+                 public void onClick(View v) {
+                     Intent intent = new Intent(context, NoteActivity.class);
+                     intent.putExtra(NoteActivity.NOTE_POSITION,currentPosition);
+                     context.startActivity(intent);
+                 }
+             });
+        }
+
+
+    }
+}
