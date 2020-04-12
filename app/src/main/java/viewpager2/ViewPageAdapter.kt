7eeff4fbc17e31e5a7a7ae9d7d9896f037a794ab
dhoyne <darrenhoyne@gmail.com>
2020-04-12 21:07:55 +0100

    View Pager with fragments and views

---
+++ app/src/main/java/viewpager2/ViewPageAdapter.kt
@@ -0,0 +1,37 @@
+package viewpager2
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.learning.R
+import kotlinx.android.synthetic.main.item_page.view.*
+
+class ViewPageAdapter(val context: Context, val itemList: List<Item>) : RecyclerView.Adapter<ViewPageAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val itemView = LayoutInflater.from(context).inflate(R.layout.item_page, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int = itemList.size
+
+    override fun onBindViewHolder(myViewHolder: MyViewHolder, position: Int) {
+
+        myViewHolder.setData(itemList[position])
+    }
+
+    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun setData(item: Item) {
+
+            itemView.txvItemNumber.text = item.itemNumber
+
+            val color = ContextCompat.getColor(context, item.colorId)
+            itemView.rootCardView.setCardBackgroundColor(color)
+        }
+    }
+}
---
+++ app/src/main/java/viewpager2/fragmentpager/MyFragmentStateAdapter.kt
@@ -0,0 +1,22 @@
+package viewpager2.fragmentpager
+
+import android.content.Context
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+
+class MyFragmentStateAdapter(context: Context, val itemList: List<Foo>) : FragmentStateAdapter(context as FragmentActivity) {
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        val foo = itemList[position]
+
+        return SlideFragment(foo)
+    }
+
+
+}
